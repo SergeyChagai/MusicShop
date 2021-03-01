d6b0631d0af6574dec9e6e3984ea95e105503564
Чагай Сергей <chagaj42@gmail.com>
2021-03-01 15:09:31 +0300

    Try to add models

--- app/src/main/java/com/example/musicshop/MainActivity.kt
+++ app/src/main/java/com/example/musicshop/MainActivity.kt
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var names = R.array.models
+    var prices = R.array.prices
+    var picSrc = R.array.picSrc
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
 
-class Model (val name: String, var price: Int) {
+class Model (val name: String, var price: Int, var picSrc: String) {
 
 }
